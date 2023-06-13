9fb95a02710178d6905f9393765c92a28a4040bb
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:50:11 +0530

    scanner

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.lang.*;
-import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        NumberFormat curr = NumberFormat.getCurrencyInstance();
-        String re= curr.format(1234567.98765);
-        System.out.println(re);
-        NumberFormat perc = NumberFormat.getPercentInstance();
-        String res = perc.format(1234567.98765);
-        System.out.println(res);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Age : ");
+        int age = sc.nextInt();
+        System.out.println("you are "+age);
+        System.out.print("Name : ");
+        String name = sc.nextLine();
+        System.out.println("you are "+name);
     }
 }
