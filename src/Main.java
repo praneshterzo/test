184227427045d3458c051a5c3129d68694d2ebeb
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:30:48 +0530

    constants

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1 ,23, 43, 0 ,0};
-        nums[3] = 37;
-        nums[4] = 7;
-        System.out.println(Arrays.toString(nums));
-        System.out.println(nums.length);
+        final float pi = 3.14F;
+        float r = 5.78F;
+        System.out.println(2*pi*r);
     }
 }
