0784b16e0bdc451565389d80bd82f22f99b0ff2b
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:58:12 +0530

    fizbiz

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 import java.lang.*;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Principle : ");
-        int principle = sc.nextInt();
-        System.out.print("\nRate of interest : ");
-        float rateOfInterest = sc.nextFloat()/100/12;
-        System.out.print("\nno. of years : ");
-        int noOfYears = sc.nextInt()*12;
-        double mortgage = ( principle * rateOfInterest * Math.pow( (1+rateOfInterest), noOfYears))/(Math.pow((1+rateOfInterest), noOfYears) - 1);
-        System.out.println("Mortgage : "+mortgage);
-        int a=(int)(Math.random()*75);
-        if(a>20){
-            System.out.println(true);
+        System.out.print("number : ");
+        int number = sc.nextInt();
+        if(number % 3 ==0 && number % 5 ==0){
+            System.out.println("FizzBuzz");
+        }
+        else if(number % 3 ==0){
+            System.out.println("Buzz");
+        }
+        else if(number % 5 ==0){
+            System.out.println("Fizz");
         }
         else{
-            System.out.println(false);
+            System.out.println(number);
         }
-        boolean res = a>50;
-        String s = a>50 ? "It is more thn 50" : "it is less than 50";
-        System.out.println(s);
     }
 }
