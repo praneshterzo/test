23fd8c6e6c447ac5ba5ef812130f00b273670dad
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:55:37 +0530

    ifelse

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Age : ");
-        int age = sc.nextInt();
-        System.out.println("you are "+age);
-        System.out.print("Name : ");
-        String name = sc.nextLine();
-        System.out.println("you are "+name);
+        System.out.print("Principle : ");
+        int principle = sc.nextInt();
+        System.out.print("\nRate of interest : ");
+        float rateOfInterest = sc.nextFloat()/100/12;
+        System.out.print("\nno. of years : ");
+        int noOfYears = sc.nextInt()*12;
+        double mortgage = ( principle * rateOfInterest * Math.pow( (1+rateOfInterest), noOfYears))/(Math.pow((1+rateOfInterest), noOfYears) - 1);
+        System.out.println("Mortgage : "+mortgage);
+        int a=(int)(Math.random()*75);
+        if(a>20){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+        boolean res = a>50;
+        String s = a>50 ? "It is more thn 50" : "it is less than 50";
+        System.out.println(s);
     }
 }
