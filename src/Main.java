e641031c2a6cbdcd8b7364e94bc56482ed1f97c3
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:34:06 +0530

    operators

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final float pi = 3.14F;
-        float r = 5.78F;
-        System.out.println(2*pi*r);
+        int a=67;
+        int b=9;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        float d= (float)5/(float)4;
+        System.out.println(d);
     }
 }
