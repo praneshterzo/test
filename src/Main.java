2c9beeba5f38cd4d1d9cd9df28bf35ea9a9ec7d5
praneshterzo <pranesh@terzocloud.com>
2023-06-13 17:05:49 +0530

    Variables

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-            int age=30;
-            long viewcount=1_345_876_987L;
-            float price=67.82F;
-            double tax=8.8765;
-            char str='P';
-
-            boolean isfunction=true;
-            System.out.println(tax);
+            Date now=new Date();
+            System.out.println(now);
         }
     }
