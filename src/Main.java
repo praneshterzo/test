c484ffddc91cb0e972c5059044bdf7b7cb64e21d
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:27:39 +0530

    arrays1d

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {1 ,23, 43, 0 ,0};
         nums[3] = 37;
-        nums[4] = 76;
+        nums[4] = 7;
         System.out.println(Arrays.toString(nums));
         System.out.println(nums.length);
     }
