5d2916a3b23f08de6d954397b8388e8ca35498d1
praneshterzo <pranesh@terzocloud.com>
2023-06-13 19:27:10 +0530

    arrays

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] nums = new int[2][2];
-        nums[1][0] = 99;
-        nums[1][1] = 42;
-        System.out.println(Arrays.deepToString(nums));
-        System.out.println(nums[0].length);
+        int[] nums = {1 ,23, 43, 0 ,0};
+        nums[3] = 37;
+        nums[4] = 76;
+        System.out.println(Arrays.toString(nums));
+        System.out.println(nums.length);
     }
 }
